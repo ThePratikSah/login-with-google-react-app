8746302ce8bf60d7531b9ca7cee4d1c58af16daf
Pratik Sah <shivam3448@gmail.com>
2020-11-07 01:28:32 +0530

    Added client id in .env file

--- src/components/Logout.jsx
+++ src/components/Logout.jsx
@@ -1,8 +1,7 @@
 import React from "react";
 import { GoogleLogout } from "react-google-login";
 
-const clientId =
-  "56109275176-3t0s4t70ot7smt8gq4jarad162uqn0vm.apps.googleusercontent.com";
+const clientId = process.env.REACT_APP_CLIENT_ID;
 
 function Logout(props) {
   function onSuccess(res) {
